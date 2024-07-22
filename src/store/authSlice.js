e9b630480e95e31011419b12b6d86d3e7af9e163
hieumih <hieuminhi2004@gmail.com>
2024-07-22 17:53:19 +0700

    sstore

---
+++ src/store/authSlice.js
@@ -0,0 +1,27 @@
+﻿import { createSlice } from '@reduxjs/toolkit'
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        user: null,
+        pass: null
+    },
+    reducers: {
+        login: (state, action) => {
+            state.user = action.payload.user
+            state.pass = action.payload.pass
+            console.log(state.user, state.pass)
+        },
+        logout: (state) => {
+            
+            console.log('logout', { user: state.user, pass: state.pass})
+            state.user = null
+            state.pass = null
+            console.log({ user: state.user, pass: state.pass})
+        }
+    }
+})
+
+export const { login, logout } = authSlice.actions
+
+export default authSlice.reducer

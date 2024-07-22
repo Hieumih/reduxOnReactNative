44c031622805dd22d1775c4f358b8d7342899223
hieumih <hieuminhi2004@gmail.com>
2024-07-22 17:53:29 +0700

    navigation

---
+++ src/navigations/index.jsx
@@ -0,0 +1,23 @@
+﻿// navigaion
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import SimpleView from './SimpleView';
+import AuthView from './AuthView';
+const Stack = createNativeStackNavigator();
+
+
+
+const StackNavigator = () => {
+    return (
+        <Stack.Navigator screenOptions={
+            {
+                headerShown: false
+            }
+        }>
+            <Stack.Screen name="Auth" component={AuthView} />
+            <Stack.Screen name="Home" component={SimpleView} />
+        </Stack.Navigator>
+    );
+}
+
+export default StackNavigator;
